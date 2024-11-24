088bb87d3ea5aaf88292cd10f6f6af185a5aafad
iliags <19849002+iliags@users.noreply.github.com>
2024-11-23 20:03:48 -0700

    Sample code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use qrcode_generator::QrCodeEcc;
+
 fn main() {
-    println!("Hello, world!");
+    std::fs::create_dir_all("test_output").unwrap_or_else(|e| {
+        eprintln!("Error creating test directory: {}", e);
+        std::process::exit(1);
+    });
+
+    let result = qrcode_generator::to_png_to_file(
+        "Hello world!",
+        QrCodeEcc::Low,
+        1024,
+        "test_output/file_output.png",
+    );
+
+    match result {
+        Ok(_) => println!("QR code generated successfully!"),
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        }
+    }
 }
